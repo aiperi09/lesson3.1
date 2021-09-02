a8747987a9fed4d1bea221576a8fb854e50c7273
aiperi09 <aiperi.miran@bk.ru>
2021-09-02 16:48:09 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        double[] day = {5.1, -3.8, -0.2, 3.3, -1.6, 5.4, 3.1, 8.2, 2.2, 5.4, 1.3, -2.5, 6.1, -8.6, 3.2};
+        double result = 0;
+        for (double a:day) {
+            result += a;
+
+        }
+        System.out.println(Arrays.toString(day) + "=" + (result/day.length + " "));
+        
+
+
+        }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
