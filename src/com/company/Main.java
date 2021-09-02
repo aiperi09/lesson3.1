633baabccd2a59742ecbbc729497debc5c1f9812
aiperi09 <aiperi.miran@bk.ru>
2021-09-02 17:53:42 +0300

    fdd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,18 +8,28 @@ public class Main {
 
     public static void main(String[] args) {
         double[] day = {5.1, -3.8, -0.2, 3.3, -1.6, 5.4, 3.1, 8.2, 2.2, 5.4, 1.3, -2.5, 6.1, -8.6, 3.2};
-        double result = 0;
-        for (double a:day) {
-            result += a;
+        for (int i = 0; i < day.length;i++) {
+            if (day[i]<0){
+                continue;
+        }
+            double result = 0;
+            for (double a:day) {
+                result +=a;
+
+
+            }
+                System.out.println(Arrays.toString(day) +"=" +(+ result / day.length));
+
 
         }
-        System.out.println(Arrays.toString(day) + "=" + (result/day.length + " "));
-        
+
+        }
+
+
 
 
         }
 
-        }
 
 
 
